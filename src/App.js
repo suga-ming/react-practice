ba19f5dcad2be2d79acee55028dba30d143bb7a0
suga-ming <kby9901@naver.com>
2022-11-14 18:31:36 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
@@ -5,23 +8,67 @@ import { QueryClient, QueryClientProvider } from "react-query";
 // import {QueryClient}
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: Infinity,
-      },
-    },
-  });
+  const [loading, setLoading] = useState(true);
+  const [coin, setCoin] = useState([]);
+
+  const coinApi = async () => {
+    try {
+      await axios.get("https://api.coinpaprika.com/v1/tickers").then((coin) => {
+        setCoin(coin.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // async () => {};
+    //  fetch("https://api.coinpaprika.com/v1/tickers")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setCoin(data);
+    //     setLoading(data);
+    // });
+    coinApi();
+  }, []);
+
+  // const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: {
+  //       staleTime: Infinity,
+  //     },
+  //   },
+  // });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/edit" element={<Edit />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <>
+      <h1>The Coins! {loading ? "" : `(${coin.length})`}</h1>
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <br />
+          <select>
+            {coin.map((item) => (
+              <option>
+                {item.name} ({item.symbol}) : ${item.quotes.USD.ath_price} USD
+              </option>
+              // <input placeholder="$20" />
+              // <span>총 {coin.quotes.USD.ath_price}</span>
+            ))}
+          </select>
+        </>
+      )}
+    </>
+    // <QueryClientProvider client={queryClient}>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/edit" element={<Edit />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </QueryClientProvider>
   );
 }
 
