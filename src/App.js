cf7f330360df48cfccbf250960457113e832f955
suga-ming <kby9901@naver.com>
2022-11-16 14:18:32 +0900

    coin

--- src/App.js
+++ src/App.js
@@ -5,78 +5,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Coin from "./practice/Coin";
 // import {QueryClient}
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coin, setCoin] = useState([]);
-  const [money, setMoney] = useState("");
-  const [selected, setSelected] = useState("");
-
-  const coinApi = async () => {
-    try {
-      await axios.get("https://api.coinpaprika.com/v1/tickers").then((coin) => {
-        setCoin(coin.data);
-        setLoading(false);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    // async () => {};
-    //  fetch("https://api.coinpaprika.com/v1/tickers")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setCoin(data);
-    //     setLoading(data);
-    // });
-    coinApi();
-  }, []);
-
-  // useEffect(() => {
-  //   console.log(selected);
-  // }, [selected]);
-
-  // const queryClient = new QueryClient({
-  //   defaultOptions: {
-  //     queries: {
-  //       staleTime: Infinity,
-  //     },
-  //   },
-  // });
-
   return (
     <>
-      <h1>The Coins! {loading ? "" : `(${coin.length})`}</h1>
-      {loading ? (
-        "loading..."
-      ) : (
-        <>
-          <br />
-          <select onChange={(e) => setSelected(e.target.value)}>
-            {coin.map((item) => (
-              <option value={item.quotes.USD.ath_price}>
-                {item.name} ({item.symbol}) : ${item.quotes.USD.ath_price} USD
-              </option>
-              // {item.quotes.USD.ath_price}
-              // <input placeholder="$20" />
-              // <span>총 {coin.quotes.USD.ath_price}</span>
-            ))}
-          </select>
-          <br />
-          <br />
-          <form>
-            내가 가지고 있는 가격:{" "}
-            <input onChange={(e) => setMoney(e.target.value)}></input>
-            <br />살 수 있는 갯수: {Math.floor(money / selected)}
-          </form>
-
-          {console.log(typeof selected)}
-          {console.log(money)}
-        </>
-      )}
+      <Coin />
     </>
     // <QueryClientProvider client={queryClient}>
     //   <BrowserRouter>
---
+++ src/practice/Coin.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Coin = () => {
+  const [loading, setLoading] = useState(true);
+  const [coin, setCoin] = useState([]);
+  const [money, setMoney] = useState("");
+  const [selected, setSelected] = useState("");
+
+  const coinApi = async () => {
+    try {
+      await axios.get("https://api.coinpaprika.com/v1/tickers").then((coin) => {
+        setCoin(coin.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // async () => {};
+    //  fetch("https://api.coinpaprika.com/v1/tickers")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setCoin(data);
+    //     setLoading(data);
+    // });
+    coinApi();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(selected);
+  // }, [selected]);
+
+  // const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: {
+  //       staleTime: Infinity,
+  //     },
+  //   },
+  // });
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coin.length})`}</h1>
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <br />
+          <select onChange={(e) => setSelected(e.target.value)}>
+            {coin.map((item) => (
+              <option value={item.quotes.USD.ath_price}>
+                {item.name} ({item.symbol}) : ${item.quotes.USD.ath_price} USD
+              </option>
+              // {item.quotes.USD.ath_price}
+              // <input placeholder="$20" />
+              // <span>총 {coin.quotes.USD.ath_price}</span>
+            ))}
+          </select>
+          <br />
+          <br />
+          <form>
+            내가 가지고 있는 가격:{" "}
+            <input onChange={(e) => setMoney(e.target.value)}></input>
+            <br />살 수 있는 갯수: {Math.floor(money / selected)}
+          </form>
+
+          {console.log(typeof selected)}
+          {console.log(money)}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Coin;
